ba1dea843b9eac82b0470964042380d0f925be3c
saddamhusainkhan <Saddamhusain.Khan@talentica.com>
2025-02-05 13:20:20 +0530

    updated application1

--- application1/src/App.js
+++ application1/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className='App'>
-      <h1>Hello, Welcome to the application1</h1>
+      <h1>Hello, Welcome to the application1 updated</h1>
     </div>
   );
 }
