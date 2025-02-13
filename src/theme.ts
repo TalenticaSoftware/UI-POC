3b7666f06c9c96c162e556479d2778e3deda4c25
palak-tal <palak.amralkar@talentica.com>
2025-02-13 12:59:44 +0530

    removed unwanted code

--- src/theme.ts
+++ src/theme.ts
@@ -2,40 +2,14 @@ import { background, border, extendTheme, ThemeConfig } from '@chakra-ui/react';
 import { mode } from '@chakra-ui/theme-tools';
 import { color } from 'framer-motion';
 
-// Define the color mode configuration
+
 const config: ThemeConfig = {
   initialColorMode: 'light',
   useSystemColorMode: false,
 };
 
-// const sizes = {
-//     ...baseTheme.space,
-//     max: "max-content",
-//     min: "min-content",
-//     full: "100%",
-//     "3xs": "14rem",
-//     "2xs": "16rem",
-//     xs: "20rem",
-//     sm: "24rem",
-//     md: "28rem",
-//     lg: "32rem",
-//     xl: "36rem",
-//     "2xl": "42rem",
-//     "3xl": "48rem",
-//     "4xl": "56rem",
-//     "5xl": "64rem",
-//     "6xl": "72rem",
-//     "7xl": "80rem",
-//     "8xl": "90rem",
-//     container: {
-//       sm: "640px",
-//       md: "768px",
-//       lg: "1024px",
-//       xl: "1280px",
-//     },
-//   };
 
-// Define custom theme colors, fonts, and component styles
+
 const theme = extendTheme({
   config,
   styles: {
