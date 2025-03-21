abf676592aacbfdd999e464fcf6005a1838a4f81
Abhishek Kumar <abhishek.kumar1@talentica.com>
2025-03-21 15:28:01 +0530

    Svelte POC

---
+++ Svelte Todo/src/App.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import '../src/styles/global.css'
+  import Todos from '../src/components/Todos.svelte'
+</script>
+
+<main>
+  <Todos/>
+</main>
---
+++ Svelte Todo/src/components/FilterTodos.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+    import type { FiltersType } from '../types/todos'
+  
+    type SetFilterType = (newFilter: FiltersType) => void
+  
+    // svelte-ignore export_let_unused
+        export let selectedFilter: FiltersType
+    // svelte-ignore export_let_unused
+        export let setFilter: SetFilterType
+    let filters = ['all', 'active', 'completed']
+</script>
+  
+<div class="filters">
+{#each filters as filter}
+    <button
+    on:click={() => setFilter(filter as FiltersType)}
+    class:selected={selectedFilter === filter}
+    class="filter"
+    >
+    {filter}
+    </button>
+{/each}
+</div>
+
+<style>
+.filters {
+    display: flex;
+    gap: var(--spacing-4);
+}
+
+.filter {
+    text-transform: capitalize;
+    padding: var(--spacing-4) var(--spacing-8);
+    border: 1px solid transparent;
+    border-radius: var(--radius-base);
+}
+
+.filter:hover {
+    border: 1px solid var(--color-highlight);
+}
+
+.selected {
+    border-color: var(--color-highlight);
+}
+</style>
